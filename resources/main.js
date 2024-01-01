e9498bb692f609fdab2167747b8a833e493239fc
bdeleonard <154763505+bdeleonard@users.noreply.github.com>
2023-12-31 21:45:06 -0500

    Uploaded contact.html

--- resources/main.js
+++ resources/main.js
@@ -3,8 +3,7 @@ function headerFollow(){
     const main = document.getElementsByTagName('main')[0];
     let y = window.scrollY;
     let width = window.innerWidth;
-    if(y > 1 && y > 43 && width >= 340) {
-        header.style.backdropFilter = 'blur(8px)';
+    if(y > 1 && y > 43 && width >= 360) {
         header.style.backgroundColor = 'rgba(0,0,0,0.7)';
         header.style.position = 'fixed';
         header.style.left = '0';
@@ -12,7 +11,7 @@ function headerFollow(){
         header.style.width = '100%';
         header.style.borderBottom = '2px solid rgba(255,255,255,0.1)';
         main.style.marginTop = '83px';
-    } else if (y > 1 && width >= 340){
+    } else if (y > 1 && width >= 360){
         header.style.position = 'fixed';
         header.style.left = '0';
         header.style.right = '0';
@@ -23,7 +22,6 @@ function headerFollow(){
         header.style.left = '';
         header.style.right = '';
         main.style.marginTop = '';
-        header.style.backdropFilter = '';
         header.style.backgroundColor = '';
     }
 }
